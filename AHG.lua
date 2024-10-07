947b3372e4ea4a9ee73d67c5ff833575427f4319
qqFoxxy <alexeifoxxy@gmail.com>
2024-10-07 09:53:36 +0300

    AHG.lua

--- AHG.lua
+++ AHG.lua
@@ -1,52 +1,58 @@
-function lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+function XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg(data) m=string.sub(data, 0, 55) data=data:gsub(m,'')
+
+data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
 
 
-local OrionLib = loadstring(game:HttpGet((lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,12075,10920,11340,10605,12600,12495,10185,11970,10605,4935,8295,11970,11025,11655,11550,4935,11445,10185,11025,11550,4935,12075,11655,12285,11970,10395,10605}))))()
-local Window = OrionLib:MakeWindow({Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7875,10605,12705,8715,12705,12075,12180,10605,11445}), HidePremium = true, SaveConfig = true, ConfigFolder = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({8295,11970,11025,11655,11550,8820,10605,12075,12180})})
+ 
+
+
+local OrionLib = loadstring(game:HttpGet((XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('uwSvoHPErFTraIgnCBUekJMZfrbzibVgOZIKhHJTeJMNMTcQlySuuyeaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NobGV4d2FyZS9Pcmlvbi9tYWluL3NvdXJjZQ=='))))()
+local Window = OrionLib:MakeWindow({Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('UPFZQoYmXcSjZASoHnqDMlbxAencqUvqtvOgRwAXVmwnisiSHWIvwejS2V5U3lzdGVt'), HidePremium = true, SaveConfig = true, ConfigFolder = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('ijVBPVTWPvoiDZHlLDVXDhKhRNPsvozRCDlHuwgtIfwftCSYMTvhGEsT3Jpb25UZXN0')})
 
 OrionLib:MakeNotification({
-	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9345,11655,12285,11970,3360,11025,11550,3465}),
-	Content = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9345,11655,12285,11970,3360,7665,11550,3360,8820,10920,10605,3360,8715,11760,10605,10395,11025,10185,11340,3360,7980,7245,6825,8190,3360,11760,10185,11970,12180,12705}),
-	Image = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
+	Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('iBqlbdWeNKrGlqtmEHDNnQqpVLVwXmuLxbUHDfnkGspFtAwIXllNdwWWW91ciBpbiE='),
+	Content = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('SuBVBQdqhDSLpdCxdiGDTOuALPTYPnRHIKVKjnAYLbXIDvVYKZbfAHUWW91ciBJbiBUaGUgU3BlY2lhbCBMRUFOIHBhcnR5'),
+	Image = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('NMJBjgHWofFbvWVtQIWtCodwnhFUqxdvxOzUwiuSsWcBuPPdpSLyFJKcmJ4YXNzZXRpZDovLzQ0ODMzNDU5OTg='),
 	Time = 5
 })
 
-_G.Key = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({8190,7245,9135}) -- You Put Your Key Here
-_G.KeyInput = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({12075,12180,11970,11025,11550,10815})
+_G.Key = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('slwXVRiNzwjhMtBJKXOyoYYCiTzOvAHhaNHwwTyavOAtmwzXxWSJVnSTkVX') -- You Put Your Key Here
+_G.KeyInput = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('OSoZzxbQPvpfaiFIcbBuIKBwstTQHwDjEgcmLKlaLRyFOagagADGJMHc3RyaW5n')
 
 function MakeScriptHub()
-loadstring(game:HttpGet((lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({})), true))() -- You Put Your Script Here
+loadstring(game:HttpGet((XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('ZvSAXlTvlzzOVimXRNsleqYkEKxuUvPwZRKczJblaHHPWGBBlzIlYNu')), true))() -- You Put Your Script Here
 
 end
 
 
 function CorrectKeyNotifications()
     OrionLib:MakeNotification({
-        Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7035,11655,11970,11970,10605,10395,12180,3360,7875,10605,12705}),
-        Content = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9135,10605,11340,10395,11655,11445,10605,3360,8820,11655,3360,8820,10920,10605,3360,7980,7245,6825,8190,3360,8400,6825,8610,8820,9345,3360,6090,7140}),
-        Image = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
+        Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('MOOsahfdlFlVAjATfsTccKUIlFjudIgCNktJikIyktOnrfyBiJnYDFnQ29ycmVjdCBLZXk='),
+        Content = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('DUgCsPqfjWFVgIBbSdEajCAlYXDAhGnDEobPbOLUJEsuPKBjZZeDqzPV2VsY29tZSBUbyBUaGUgTEVBTiBQQVJUWSA6RA=='),
+        Image = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('koQQGQkmorJqxGseMKHpzzabvsTpxBXAmCXpKkuQSFtOBboDApDADhXcmJ4YXNzZXRpZDovLzQ0ODMzNDU5OTg='),
         Time = 5
     })
 end
 
 function WrongKeyNotifications()
     OrionLib:MakeNotification({
-        Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({3360,9135,11970,11655,11550,10815,3360,7875,10605,12705}),
-        Content = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9345,11655,12285,11970,3360,8190,11655,12180,3360,11655,11550,3360,8820,10920,10605,3360,7980,11025,12075,12180,3360,7875,11025,10500}),
-        Image = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
+        Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('WxWsVHFMtboYThjXGHGwJeccdJntWBzpgkODeDAqAkNyYCbhGlpvgjSIFdyb25nIEtleQ=='),
+        Content = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('wcrFWpnysgbpIuRrtpCjwtulklWWOiXrDDTBPqdeTGhCExedpEGGvahWW91ciBOb3Qgb24gVGhlIExpc3QgS2lk'),
+        Image = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('JbgzwQOnOMujebNIILNBdANkwBFstJNPTJVUlDtOnOFvnyVmmwgRaljcmJ4YXNzZXRpZDovLzQ0ODMzNDU5OTg='),
         Time = 5
     })
 end
 
 local Tab = Window:MakeTab({
-	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7875,10605,12705,3360,8610,11655,11655,11445}),
-	Icon = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
+	Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('hkTSuPGUWfhwEBiylSAgChievIAirhbCfZRwadjtPvfuyQaYZKOIjfdS2V5IFJvb20='),
+	Icon = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('hzyzgkjnxGWvnVagwVLrZKkUBKTiXbTopIDtNanEriwVQObXkbSTtIlcmJ4YXNzZXRpZDovLzQ0ODMzNDU5OTg='),
 	PremiumOnly = false
 })
 
 Tab:AddTextbox({
-	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7245,11550,12180,10605,11970,3360,7875,10605,12705}),
-	Default = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7245,11550,12180,10605,11970,3360,7875,10605,12705}),
+	Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('DjFeIefIxvMFattBNhOblkkePuegrFjkfdNOpJFSVRAuPwdbUepeHRHRW50ZXIgS2V5'),
+	Default = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('kNQFaVdryaArKVrXhqOwLueGQDkNYjxbvSPbbLmhddsCDepyDVRDaWPRW50ZXIgS2V5'),
 	TextDisappear = true,
 	Callback = function(Value)
         _G.KeyInput = Value
@@ -54,7 +60,7 @@ Tab:AddTextbox({
 })
 
 Tab:AddButton({
-	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7035,10920,10605,10395,11235,3360,7875,10605,12705,3465}),
+	Name = XuGSIsOkfwAmOAQxZEWrQjkrAVdSAnqbvpnFkZioZYuHtLTKldmMlCSXIBsUBcTg('HhLbUCQSXVIKiMktKJaRCQqszGKNXLjTKMmAJdAtmMGzPryzoiAQTdHQ2hlY2sgS2V5IQ=='),
 	Callback = function()
         if _G.KeyInput == _G.Key then
          MakeScriptHub()
