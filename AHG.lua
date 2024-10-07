9f00677068aef7b7381f708181ddf6ceaf52ef34
qqFoxxy <alexeifoxxy@gmail.com>
2024-10-07 09:48:15 +0300

    AHG.lua

--- AHG.lua
+++ AHG.lua
@@ -1,49 +1,52 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "KeySystem", HidePremium = true, SaveConfig = true, ConfigFolder = "OrionTest"})
+function lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+local OrionLib = loadstring(game:HttpGet((lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,12075,10920,11340,10605,12600,12495,10185,11970,10605,4935,8295,11970,11025,11655,11550,4935,11445,10185,11025,11550,4935,12075,11655,12285,11970,10395,10605}))))()
+local Window = OrionLib:MakeWindow({Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7875,10605,12705,8715,12705,12075,12180,10605,11445}), HidePremium = true, SaveConfig = true, ConfigFolder = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({8295,11970,11025,11655,11550,8820,10605,12075,12180})})
 
 OrionLib:MakeNotification({
-	Name = "Your in!",
-	Content = "Your In The Special LEAN party",
-	Image = "rbxassetid://4483345998",
+	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9345,11655,12285,11970,3360,11025,11550,3465}),
+	Content = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9345,11655,12285,11970,3360,7665,11550,3360,8820,10920,10605,3360,8715,11760,10605,10395,11025,10185,11340,3360,7980,7245,6825,8190,3360,11760,10185,11970,12180,12705}),
+	Image = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
 	Time = 5
 })
 
-_G.Key = "NEW" -- You Put Your Key Here
-_G.KeyInput = "string"
+_G.Key = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({8190,7245,9135}) -- You Put Your Key Here
+_G.KeyInput = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({12075,12180,11970,11025,11550,10815})
 
 function MakeScriptHub()
-loadstring(game:HttpGet((""), true))() -- You Put Your Script Here
+loadstring(game:HttpGet((lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({})), true))() -- You Put Your Script Here
 
 end
 
 
 function CorrectKeyNotifications()
     OrionLib:MakeNotification({
-        Name = "Correct Key",
-        Content = "Welcome To The LEAN PARTY :D",
-        Image = "rbxassetid://4483345998",
+        Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7035,11655,11970,11970,10605,10395,12180,3360,7875,10605,12705}),
+        Content = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9135,10605,11340,10395,11655,11445,10605,3360,8820,11655,3360,8820,10920,10605,3360,7980,7245,6825,8190,3360,8400,6825,8610,8820,9345,3360,6090,7140}),
+        Image = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
         Time = 5
     })
 end
 
 function WrongKeyNotifications()
     OrionLib:MakeNotification({
-        Name = " Wrong Key",
-        Content = "Your Not on The List Kid",
-        Image = "rbxassetid://4483345998",
+        Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({3360,9135,11970,11655,11550,10815,3360,7875,10605,12705}),
+        Content = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({9345,11655,12285,11970,3360,8190,11655,12180,3360,11655,11550,3360,8820,10920,10605,3360,7980,11025,12075,12180,3360,7875,11025,10500}),
+        Image = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
         Time = 5
     })
 end
 
 local Tab = Window:MakeTab({
-	Name = "Key Room",
-	Icon = "rbxassetid://4483345998",
+	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7875,10605,12705,3360,8610,11655,11655,11445}),
+	Icon = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
 	PremiumOnly = false
 })
 
 Tab:AddTextbox({
-	Name = "Enter Key",
-	Default = "Enter Key",
+	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7245,11550,12180,10605,11970,3360,7875,10605,12705}),
+	Default = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7245,11550,12180,10605,11970,3360,7875,10605,12705}),
 	TextDisappear = true,
 	Callback = function(Value)
         _G.KeyInput = Value
@@ -51,7 +54,7 @@ Tab:AddTextbox({
 })
 
 Tab:AddButton({
-	Name = "Check Key!",
+	Name = lxKjCtnkPNfBHCJGwnUaZMVmhhFftcBvewNuggbXBcwERcbgJunkYkerMsERzIKdhcSV({7035,10920,10605,10395,11235,3360,7875,10605,12705,3465}),
 	Callback = function()
         if _G.KeyInput == _G.Key then
          MakeScriptHub()
@@ -60,4 +63,4 @@ Tab:AddButton({
         WrongKeyNotifications()
         end
   	end    
-})
+})    
